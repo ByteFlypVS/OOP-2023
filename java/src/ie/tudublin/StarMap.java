499910c6d6798eca8338dba85f782e2078dde6ba
C21344786 Shawn Lorenzo <C21344786@mydit.ie>
2023-02-15 16:29:45 +0000

    killing the cat

--- java/src/ie/tudublin/StarMap.java
+++ java/src/ie/tudublin/StarMap.java
@@ -1,45 +1,57 @@
 package ie.tudublin;
 
 import processing.core.PApplet;
+import processing.data.Table;
+import processing.data.TableRow;
 
 public class StarMap extends PApplet
 {
 	public void settings()
 	{
-		size(500, 500);
+		size(800, 800);
 	}
 
-	public void setup() {
+	public void setup()
+	{
 		colorMode(HSB);
 		background(0);
-		
-		smooth();
-		
-
-
 	}
-
-
+	
 	public void drawGrid()
 	{
-		stroke(255);
-		float border = 50.0f;
-
 		int count = 10;
+		float border = 50.0f;
 		float gap = (width - (border * 2.0f)) / (float) count;
+
 		for(int i = -5 ; i <= 5 ; i ++)
 		{
 			float x = border + (gap * (i + 5));
+
+			stroke(210, 255, 255);
 			line(x, border, x, height - border);
 			line(border, x, width - border, x);
+
+			textSize(11);
+			fill(210, 255, 255);
+			text(i, x, border-20);
+			text(i, border-30, x);
 		}
-		
+	}
+
+	void loadStars()
+	{
+		Table table = loadTable("HabHYG15ly.csv", "header");
+
+		for(TableRow r:table.rows())
+		{
+			Star s = new Star(r);
+			stars.add(s);
+		}
 	}
 		
 	public void draw()
-	{	
-		strokeWeight(2);		
-
+	{
 		drawGrid();
+		loadStars();
 	}
 }
