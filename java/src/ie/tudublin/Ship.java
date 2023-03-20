44c27a17ad2fc27485f17af491d9c5268d2cf8de
Shawn Lorenzo <c21344786@mytudublin.ie>
2023-03-20 12:09:05 +0000

    shooting asteroids

--- java/src/ie/tudublin/Ship.java
+++ java/src/ie/tudublin/Ship.java
@@ -3,7 +3,8 @@ package ie.tudublin;
 import processing.core.PApplet;
 import processing.core.PVector;
 
-public class Ship {
+public class Ship
+{
     private PVector pos;
     private PApplet p;
 
@@ -15,40 +16,54 @@ public class Ship {
         this.p = p;  
     }
 
-
-
-    public PVector getPos() {
+    public PVector getPos()
+    {
         return pos;
     }
-    public void setPos(PVector pos) {
+
+    public void setPos(PVector pos)
+    {
         this.pos = pos;
     }
-    public float getRot() {
+
+    public float getRot()
+    {
         return rot;
     }
-    public void setRot(float rot) {
+
+    public void setRot(float rot)
+    {
         this.rot = rot;
     }
-    public int getC() {
+
+    public int getC()
+    {
         return c;
     }
-    public void setC(int c) {
+
+    public void setC(int c)
+    {
         this.c = c;
     }
-    public float getSize() {
+
+    public float getSize()
+    {
         return size;
     }
-    public void setSize(float size) {
+
+    public void setSize(float size)
+    {
         this.size = size;
     }
+
     private float rot;
     private int c;
     private float size;
 
+    public void move()
+    {}
+
     public void render()
     {
     }
-
-
-
 }
