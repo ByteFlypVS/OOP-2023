16ae59cad82b39b1e700d76be997392c3ed2ecd5
Shawn Lorenzo <c21344786@mytudublin.ie>
2023-03-23 08:58:24 +0000

    scanning the seas

--- java/src/ie/tudublin/RadarSonar.java
+++ java/src/ie/tudublin/RadarSonar.java
@@ -5,11 +5,48 @@ import processing.core.PApplet;
 public class RadarSonar extends PApplet
 {
     public void settings()
-    {}
+    {
+        size(500, 500);
+        //fullScreen();
+    }
 
     public void setup()
     {}
 
     public void draw()
-    {}
+    {
+        int cuts = 8; 
+
+        float cX = width/2;
+        float cY = height/2;
+        float r = 0;
+
+        background(0);
+
+        // Radar base screen
+        for(int i = 1; i <= 8; i++)
+        {
+            circle(cX, cY, i*(height/10));
+            stroke(255);
+            noFill();
+
+            // Maximum radius of screen, for use below
+            if(i == 8)
+            {
+                r = (i*(height/10))/2;
+            }
+        }
+
+        // Radar grid pattern
+        /*
+        for(int i = 1; i <= cuts; i++)
+        {
+            float A = (360/cuts)*i;
+
+            line(cX, cY, cX+r*cos(A), cY+r*sin(A));
+        }
+        */
+
+        line(cX, cY, cX, cY-r);
+    }
 }
