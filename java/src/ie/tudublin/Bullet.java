5aa07359e489f7aa50cc1c3af363aaf64cc84d79
Shawn Lorenzo <c21344786@mytudublin.ie>
2023-03-27 11:54:15 +0100

    loading ammunition

---
+++ java/src/ie/tudublin/Bullet.java
@@ -0,0 +1,37 @@
+package ie.tudublin;
+
+import processing.core.PApplet;
+import processing.core.PVector;
+
+public class Bullet
+{
+    PVector position;
+    PVector forward;
+    float rotation;
+    float speed;
+    PApplet p;
+
+    public Bullet(float x, float y, float rotation, PApplet p)
+    {
+        position = new PVector(x, y);
+        forward = new PVector(0, -1);
+        speed = 5;
+        this.p = p;
+        this.rotation = rotation;
+    }
+    
+    void render()
+    {
+        p.pushMatrix();
+        p.translate(position.x, position.y);
+        p.rotate(rotation);
+        p.line(0, -5, 0, 5);
+        p.popMatrix();
+    }
+
+    void move()
+    {
+        forward.x = PApplet.sin(rotation);
+        forward.y = PApplet.cos(rotation);
+    }
+}
